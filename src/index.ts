298eb4d187924e2ab80b8c990f32170b4515795f
tcfplayz <58693033+tcfplayz@users.noreply.github.com>
2025-04-25 02:28:14 +0800

    v1.1 - cros

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from "hono/cors";
 import { xai } from '@ai-sdk/xai';
 import { generateText } from 'ai';
 
@@ -9,8 +10,22 @@ app.post('/', async (c) => {
     model: xai('grok-3-mini'),
     prompt: await c.req.text(),
   });
+
+
   return c.text(text.text, 200);
 })
 app.get('/', async (c) => c.text('fireflies-backend', 200))
+
+app.use(
+    "*",
+    cors({
+      origin: "https://fireflies.chiculture.org.hk/",
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["POST", "GET", "OPTIONS"],
+      exposeHeaders: ["Content-Length"],
+      maxAge: 600,
+      credentials: true,
+    }),
+);
 
 export default app
