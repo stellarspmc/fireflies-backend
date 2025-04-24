e30958621e7429427e79729c9c4daba5fd3fcba3
tcfplayz <58693033+tcfplayz@users.noreply.github.com>
2025-04-25 02:48:11 +0800

    v1.2 - just a simple cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Hono } from 'hono';
-import { cors } from "hono/cors";
 import { xai } from '@ai-sdk/xai';
 import { generateText } from 'ai';
 
-const app = new Hono()
+const app = new Hono();
 
 app.post('/', async (c) => {
   const text  = await generateText({
@@ -15,6 +14,9 @@ app.post('/', async (c) => {
     "Access-Control-Allow-Origin": "https://fireflies.chiculture.org.hk"
   });
 })
-app.get('/', async (c) => c.text('fireflies-backend', 200))
 
-export default app
+app.get('/', async (c) => c.text('fireflies-backend', 200, {
+  "Access-Control-Allow-Origin": "*"
+}));
+
+export default app;
